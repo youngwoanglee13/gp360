98ee08a2daca56a2778bf20093c30c2579a72e17
younglee <young.lee@ucb.edu.bo>
2024-02-28 21:45:53 -0400

    feat:add cliente component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { ClienteComponent } from './cliente/cliente.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path : '', redirectTo: '/inicio', pathMatch: 'full' },
+    { path : 'inicio', component: InicioComponent },
+    { path : 'inicio/:nombre', component: ClienteComponent }
+];
